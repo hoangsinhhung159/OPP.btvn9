f88c5c248f1e86c6f833e239554bba11d1451aa1
hoangsinhhung159 <hoangsinhhung12@gmail.com>
2026-04-09 09:37:34 +0700

    Add files via upload

---
+++ exercise 3.3.py
@@ -0,0 +1,16 @@
+def ve_dong_ngang():
+    print('+ - - - - + - - - - +')
+def ve_dong_doc():
+    print('|         |         |')
+def ve_hinh():
+    ve_dong_ngang()
+    ve_dong_doc()  
+    ve_dong_doc()
+    ve_dong_doc()   
+    ve_dong_doc()
+def ve_hinh_2x2():
+    ve_hinh()
+    ve_hinh()
+    ve_dong_ngang()
+ve_hinh_2x2()
+        
